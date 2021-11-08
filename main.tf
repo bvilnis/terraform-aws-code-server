3d2afa04ab90fd627fe5b496bd1c9266cd2f0b69
MH4GF <h.miyagi.cnw@gmail.com>
2021-11-08 16:56:53 +0900

    Change subnet_ids to subnet_id in ec2-instance module
    because ec2-instance module no longer accepts subnet_ids variables

--- main.tf
+++ main.tf
@@ -83,7 +83,7 @@ module "ec2_instance" {
   name                        = var.hostname
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_size
-  subnet_ids                  = module.vpc.public_subnets
+  subnet_id                   = module.vpc.public_subnets[0]
   vpc_security_group_ids      = [module.security_group.this_security_group_id]
   associate_public_ip_address = true
   user_data                   = data.template_file.user_data.rendered
